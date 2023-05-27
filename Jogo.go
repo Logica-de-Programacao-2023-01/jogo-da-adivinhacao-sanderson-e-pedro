2c19f5e5bc25b4548a7502baa7ef26baad2d775c
Sanderson Machado <sandersonoficial10@hotmail.com>
2023-05-27 20:12:45 -0300

    99%

--- Jogo.go
+++ Jogo.go
@@ -6,20 +6,18 @@ import (
 	"time"
 )
 
-func addy(y *[]int, tentativas int) {
-	*y = append(*y, tentativas)
-}
-
 func main() {
 	rand.Seed(time.Now().Unix())
-	var tentativas int
+	var tentativas int = 0
+	var rodada int
 	numero := rand.Intn(100) + 1
-	y := []int{}
+	y := make([]int, rodada)
 
 	for {
 		var num int
 		fmt.Println("Digite um número de 1 a 100: ")
 		fmt.Scanln(&num)
+		tentativas++
 		if num < numero {
 			fmt.Println("O número certo é maior")
 		} else if num > numero {
@@ -27,25 +25,25 @@ func main() {
 		} else {
 			fmt.Println("Você acertou!!")
 			fmt.Println("Tentativas: ", tentativas)
-			addy(&y, tentativas)
+			y = append(y, tentativas)
 			break
 			//Professor se tentativas não incluir o acerto, está certo, se não, só colocar tentativa + 1
 		}
-		tentativas++
 	}
+	rodada++
 
 	var novamente string
 	fmt.Print("Deseja jogar novamente? (n/N = Não || s/S = Sim): ")
 	fmt.Scanln(&novamente)
-
 	if novamente != "s" && novamente != "S" {
 		fmt.Println("RESUMO:")
-		for i, s := range y {
-			fmt.Printf("JOGADA: %d | TENTIVAS: %d\f", i+1, s)
-			i++
-		}
 	} else {
 		main()
+	}
+
+	for i, s := range y {
+		fmt.Printf("JOGADA: %d | TENTIVAS: %d\f", i+1, s)
+		i++
 	}
 
 }
